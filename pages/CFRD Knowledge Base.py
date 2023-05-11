e1eb3b56985cf9ed104102b3a2e006332a7b0c54
Juan Tello <jstello@gmail.com>
2023-05-11 07:56:56 -0500

    working kb

--- pages/CFRD Knowledge Base.py
+++ pages/CFRD Knowledge Base.py
@@ -3,22 +3,54 @@ import streamlit as st
 st.title("CFRD Knowledge Base")
 st.info("""I am a knowledgeable chatbot with extensive information about Concrete Face Rockfill Dams (CFRDs). 
         I can answer questions about CFRDs, and I can also summarize the contents of a PDF file.""")
-# set up pinecone
-import pinecone
-import os
-from tqdm.auto import tqdm
 from langchain.embeddings.openai import OpenAIEmbeddings
-
-pinecone.init(api_key='48640420-7e79-46d4-b71d-d07286818fef', environment='us-central1-gcp')
-
-index_name = 'icold'
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Pinecone
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import UnstructuredPDFLoader
+import os
 embeddings = OpenAIEmbeddings()
+import pinecone 
+import os
+from tqdm.autonotebook import tqdm
+# initialize pinecone
+pinecone.init(api_key="48640420-7e79-46d4-b71d-d07286818fef",
+              environment="us-central1-gcp")
 
-index = pinecone.Index(index_name, embeddings)
+def pretty_print(response):
+    import textwrap
+    # Split the response by lines of max 80 characters
+    return '\n'.join(textwrap.wrap(response, 80))
 
-query = st.text_input("Ask me a question about CFRDs", "")
-query = "how to estimate leakage through a cfrd?"
-if query != "":
-    results = index.query(queries=[query], top_k=1)
-    st.write(results[0].text)
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+
+temperature = st.slider("Temperature", 0.0, 1.0, 0.5, 0.1)
+llm=ChatOpenAI(temperature=temperature, max_tokens=2000)
+
+docsearch = Pinecone.from_existing_index('icold', embeddings, 
+                                         )
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+#     chain_type="map_reduce",
+    chain_type="stuff",
+    retriever=docsearch.as_retriever(),
+    return_source_documents=True,
+    verbose=True,
+    )
+
+query = st.text_input("Ask me a question about CFRDs", "What is a CFRD?")
+@st.cache_data()
+def run_query(query):
+    result = qa({"query": query, "top_k": 5, "max_tokens": 2000})
+    return result["result"], result["source_documents"]
+
+if st.button("Ask"):
+        result, sources = run_query(query)
+        st.success(result)
+        # Print sources
+        for source in sources:
+            file_name = source.metadata['source']
+            file_name = os.path.basename(file_name)
+            with st.expander("Source: " + file_name):
+                st.write(source.page_content.replace("\n", " "))
